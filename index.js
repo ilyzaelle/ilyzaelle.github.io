2f6f29975088d2202b6e3441574373db362491ba
ilyzaelle <leo.rodriguez@etu.unice.fr>
2025-09-19 11:33:33 +0200

    debug JS

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function(){
     navigator.geolocation.getCurrentPosition(function(pos){
         var coords = pos.coords;
 
-        coords.latitude = null ? document.getElementById("lat").innerText = "null" : document.getElementById("lat").innerText = coords.latitude;
-        coords.longitude = null ? document.getElementById("lon").innerText = "null" : document.getElementById("lon").innerText = coords.longitude;
-        coords.altitude = null ? document.getElementById("alt").innerText = "null" : document.getElementById("alt").innerText = coords.altitude;
-        coords.speed = null ? document.getElementById("vit").innerText = "null" : document.getElementById("vit").innerText = coords.speed;
+        coords.latitude = null ? document.getElementById("lat").innerText = "N/A" : document.getElementById("lat").innerText = coords.latitude;
+        coords.longitude = null ? document.getElementById("lon").innerText = "N/A" : document.getElementById("lon").innerText = coords.longitude;
+        coords.altitude = null ? document.getElementById("alt").innerText = "N/A" : document.getElementById("alt").innerText = coords.altitude;
+        coords.speed = null ? document.getElementById("vit").innerText = "N/A" : document.getElementById("vit").innerText = coords.speed;
         console.log(coords);
     });
 })
