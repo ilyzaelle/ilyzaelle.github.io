b58c45ee9d4cb79e308ec92bb23068a025f94877
ilyzaelle <leo.rodriguez@etu.unice.fr>
2025-09-19 11:55:17 +0200

    debug JS

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const lon = document.getElementById("lon");
 const lat = document.getElementById("lat");
 const alt = document.getElementById("alt");
-const acc = document.getElementById("acc");
 const speed = document.getElementById("speed");
-const time = document.getElementById("time");
-const logEl = document.getElementById("log");
 
 if (navigator.geolocation){
     navigator.geolocation.watchPosition(function(pos){
         const c = position.coords;
         lon.textContent = (c.longitude ?? '-');
         lat.textContent = (c.latitude ?? '-');
-        alt.textContent = (c.altitude == null ? '-' : c.altitude);
-        acc.textContent = (c.accuracy ?? '-');
+        alt.textContent = (c.altitude ?? '-');
         speed.textContent = (c.speed ?? '-');
-        time.textContent = new Date(position.timestamp).toLocaleString();
     }, function(err){}, { enableHighAccuracy: true, timeout: 10000, maximumAge: 0 });
 }
